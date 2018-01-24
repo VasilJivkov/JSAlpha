612a4797ea7b856c56e4d1164ff5a62cca447c3b
[VasilJivkov] <vasil_jivkov@hotmail.com>
2018-01-24 13:46:03 +0200

    original first submission
    Just to see where the extra level of difficulty is coming from, added commented 6/10 solution. failing at string size -> reapeat(k)

--- DSA-prep/DigitSumWords.js
+++ DSA-prep/DigitSumWords.js
@@ -9,7 +9,7 @@ function digitSum(n, k) {
   if(numberStr.length <= 1){
     numberStr = n.toString().repeat(k).split('').map(Number);
     // as the repeat of K can exceed the string lenght limit at the beginnig,
-    // I add the K at the end of the recursion and manually hardcode the last
+    // I add the K at the end of the recursion and manually hardcode the last calculation after the repeat.
     sum = numberStr.reduce((a ,b) =>(a + b),0);
     numberStr = sum.toString().split('').map(Number);
     sum = numberStr.reduce((a ,b) =>(a + b),0);
@@ -23,3 +23,14 @@ function digitSum(n, k) {
 
 let result = digitSum(n ,k);
 console.log(result);
+
+// first solution, passing 6 tests,the other 4 exeeding string lenth on repeat.
+// function digitSum(n, k) {
+//   let numberStr = n.toString().repeat(k).split('').map(Number);
+//   let sum = numberStr.reduce((a ,b) =>(a + b),0);
+//   if(numberStr.length <= 1){
+//     return sum;
+//   }
+//   let result = digitSum(sum, k);
+//   return result;
+// }
