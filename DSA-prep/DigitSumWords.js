5d5ff163dd1d80c51423894ba5b096c5cadf89b3
[VasilJivkov] <vasil_jivkov@hotmail.com>
2018-01-24 13:43:00 +0200

    RecursiveSumOfDigits
    HackerRank Medium difficulty Recursion problem. Half

---
+++ DSA-prep/DigitSumWords.js
@@ -0,0 +1,25 @@
+let n_temp = ('148 3').split(' ');
+let n = n_temp[0];
+let k = parseInt(n_temp[1]);
+
+function digitSum(n, k) {
+  let numberStr = n.toString().split('').map(Number);
+  let sum = numberStr.reduce((a ,b) =>(a + b),0);
+
+  if(numberStr.length <= 1){
+    numberStr = n.toString().repeat(k).split('').map(Number);
+    // as the repeat of K can exceed the string lenght limit at the beginnig,
+    // I add the K at the end of the recursion and manually hardcode the last
+    sum = numberStr.reduce((a ,b) =>(a + b),0);
+    numberStr = sum.toString().split('').map(Number);
+    sum = numberStr.reduce((a ,b) =>(a + b),0);
+    return sum;
+  }
+
+  let result = digitSum(sum, k);
+
+  return result;
+}
+
+let result = digitSum(n ,k);
+console.log(result);
